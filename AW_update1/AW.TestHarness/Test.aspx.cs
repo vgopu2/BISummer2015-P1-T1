e91a9ba178e69be4b32301fbe69264c74e1122f2
Jindam <sumanjindam007@gmail.com>
2015-06-27 14:07:28 -0400

    Update1

---
+++ AW_update1/AW.TestHarness/Test.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using AW.Framework;
+using AW.model;
+
+
+namespace AW.TestHarness
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            EntityManager<ProductDescription> mgrProduct = new EntityManager<ProductDescription>();
+            List<ProductDescription> list1 = mgrProduct.GetAll();
+           
+            GridView1.DataSource = list1;
+            GridView1.DataBind();
+        }
+    }
+}
