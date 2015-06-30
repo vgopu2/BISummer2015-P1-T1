f0910547e34649b04d9eb9893819e9518141cf5a
muralikrishh <murali5696@gmail.com>
2015-06-30 13:29:16 -0400

    LoginForm

---
+++ LoginForm/LoginForm.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LoginForm.aspx.cs" Inherits="AW.TestHarness.LoginForm" %>
+
+
+
+   
+        
+       <%-- <asp:Login ID="ctrlLogin" runat="server" OnAuthenticate="AuthenticateUser" BackColor="#F7F6F3" BorderColor="#E6E2D8" BorderPadding="4" BorderStyle="Solid" BorderWidth="1px" Font-Names="Verdana" Font-Size="0.8em" ForeColor="#333333">
+        <InstructionTextStyle Font-Italic="True" ForeColor="Black" />
+        <LoginButtonStyle BackColor="#FFFBFF" BorderColor="#CCCCCC" BorderStyle="Solid" BorderWidth="1px" Font-Names="Verdana" Font-Size="0.8em" ForeColor="#284775" />
+        <TextBoxStyle Font-Size="0.8em" />
+        <TitleTextStyle BackColor="#5D7B9D" Font-Bold="True" Font-Size="0.9em" ForeColor="White" />
+    </asp:Login>
+   --%>
+<form id="form1" runat="server">
+    <asp:Login ID="ctrlLogin" runat="server" onAuthenticate="AuthenticateUser">
+    </asp:Login>
+</form>
---
+++ LoginForm/LoginForm.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace AW.TestHarness
+{
+    public partial class LoginForm : System.Web.UI.Page
+    {
+        string connection = ConfigurationManager.ConnectionStrings["AdventureWorksLT2012ConnectionString"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Username"] != null)
+            {
+                FormsAuthentication.RedirectFromLoginPage(Session["Username"].ToString(), false);
+            }
+        }
+
+        protected void AuthenticateUser(object sender, AuthenticateEventArgs e)
+        {
+            SqlConnection con = new SqlConnection(connection);
+            SqlCommand cmd = new SqlCommand("spAuthenticateUser", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@email", ctrlLogin.UserName);
+            cmd.Parameters.AddWithValue("@password", ctrlLogin.Password);
+            con.Open();
+            //SqlDataReader dr = cmd.ExecuteReader();
+            int value = Convert.ToInt32(cmd.ExecuteScalar());
+            con.Close();
+            if (value == 1)
+            {
+                Session["Username"] = ctrlLogin.UserName;
+                FormsAuthentication.RedirectFromLoginPage(ctrlLogin.UserName, false);
+            }
+
+
+
+            /*  if (FormsAuthentication.Authenticate(ctrlLogin.UserName, ctrlLogin.Password))
+              {
+                  Session.Add("UserName", ctrlLogin.UserName);
+                  FormsAuthentication.RedirectFromLoginPage(ctrlLogin.UserName, false);
+              }*/
+
+        }
+
+    }
+}
